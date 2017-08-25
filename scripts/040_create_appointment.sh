05554c82e64ef24829ecdba3c9dc612bbdb795ac
slayTheSensei <kylejpearson@gmail.com>
2017-08-25 18:43:45 -0400

    WIP
    Fixed broken curl scripts

--- scripts/040_create_appointment.sh
+++ scripts/040_create_appointment.sh
@@ -2,7 +2,11 @@ curl --include --request POST http://localhost:4741/appointments \
   --header "Content-Type: application/json" \
   --data '{
     "appointment": {
-      "doctors_id": "1",
-      "patients_id": "1"
+      "app_date": "2017-09-01 11:00:00",
+      "app_type": "Renewal",
+      "doctor_id": "1",
+      "patient_id": "1"
     }
   }'
+
+echo
---
+++ scripts/060_delete_appointments.sh
@@ -0,0 +1,2 @@
+curl --include --request DELETE http://localhost:4741/appointments/1Users \
+  --header "Content-Type: application/json" \
